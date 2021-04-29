356f91cccd7d29b6be588027b08f766bba3620f2
InKyeongBae <hwang0809@cau.ac.kr>
2021-04-29 13:34:37 +0900

    [Erroer] test_NullPointError 발생

--- src/test/java/com/example/digging/domain/repository/UserRepositoryTest.java
+++ src/test/java/com/example/digging/domain/repository/UserRepositoryTest.java
@@ -1,7 +1,10 @@
 package com.example.digging.domain.repository;
 
 import com.example.digging.domain.entity.User;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
 
 public class UserRepositoryTest {
 
@@ -9,9 +12,20 @@ public class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Test
     public void create(){
         User user = new User();
-        user.setId();
-        user.set
+        user.setId(1);
+        user.setCreatedAt(LocalDateTime.now());
+        user.setEmail("digging@gmail.com");
+        user.setPassword("password");
+        user.setUsername("testuser1");
+        user.setProvider("admin");
+        user.setRole("admin");
+        user.setInterest("develop");
+        user.setUpdatedAt(LocalDateTime.now());
+
+        User newUser = userRepository.save(user);
+
     }
 }
