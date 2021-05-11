235b617a9abd5cf45742176a6dc0abd1df4d1ca5
InKyeongBae <hwang0809@cau.ac.kr>
2021-05-11 22:27:28 +0900

    [FIX] error 해결

--- src/test/java/com/example/digging/domain/repository/UserRepositoryTest.java
+++ src/test/java/com/example/digging/domain/repository/UserRepositoryTest.java
@@ -1,12 +1,13 @@
 package com.example.digging.domain.repository;
 
+import com.example.digging.DiggingApplicationTests;
 import com.example.digging.domain.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDateTime;
 
-public class UserRepositoryTest {
+public class UserRepositoryTest extends DiggingApplicationTests {
 
     // Dependency Injection
     @Autowired
@@ -19,20 +20,21 @@ public class UserRepositoryTest {
 
         user.setEmail("gmail");
         user.setPassword("pass");
-        user.setUsername("test");
+        user.setUsername("test2");
         user.setProvider("admin");
         user.setRole("admin");
         user.setInterest("dev");
         user.setCreatedAt(LocalDateTime.now());
+        user.setUpdatedAt(LocalDateTime.now());
         System.out.println(user);
 
         User newUser = userRepository.save(user);
 
     }
 
-    public void read(){
-        Optional<User> user = userRepository.findAll();
-
-        user.ifPresent(user ->)
-    }
+//    public void read(){
+//        Optional<User> user = userRepository.findAll();
+//
+//        user.ifPresent(user ->)
+//    }
 }
