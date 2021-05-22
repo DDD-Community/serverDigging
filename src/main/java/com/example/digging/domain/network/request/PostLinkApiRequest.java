b662cffcbb4e133d447b1795c00b5300c6edb96d
InKyeongBae <hwang0809@cau.ac.kr>
2021-05-22 15:04:25 +0900

    [DELETE] Posts 관련 파일들

--- src/main/java/com/example/digging/domain/network/request/PostLinkApiRequest.java
+++ src/main/java/com/example/digging/domain/network/request/PostLinkApiRequest.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -14,4 +16,5 @@ public class PostLinkApiRequest {
     private Integer userId;
     private String title;
     private String url;
+    private List tags;
 }
