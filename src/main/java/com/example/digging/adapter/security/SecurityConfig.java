c02a562b21ff9f4518fc1f2bcde0ab86385740e9
InKyeongBae <hwang0809@cau.ac.kr>
2021-09-09 01:37:03 +0900

    [FIX] gradle sync failed error

--- src/main/java/com/example/digging/adapter/security/SecurityConfig.java
+++ src/main/java/com/example/digging/adapter/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.digging.adapter.security;
 
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 @EnableWebSecurity
 public class SecurityConfig {
 
