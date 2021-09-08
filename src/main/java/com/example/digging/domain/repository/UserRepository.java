10d0eae672432b16ec5185bc03239a4a55b46c88
InKyeongBae <hwang0809@cau.ac.kr>
2021-09-09 06:41:05 +0900

    [FIX] findById 수정

--- src/main/java/com/example/digging/domain/repository/UserRepository.java
+++ src/main/java/com/example/digging/domain/repository/UserRepository.java
@@ -12,5 +12,4 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     @EntityGraph(attributePaths = "authorities")
     Optional<User> findOneWithAuthoritiesByUsername(String username);
 
-    Optional<User> findByUserId(Integer userid);
 }
