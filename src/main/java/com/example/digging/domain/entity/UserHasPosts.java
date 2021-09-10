4d375350085d8d1b684e832e669abdf9c68acbd3
InKyeongBae <hwang0809@cau.ac.kr>
2021-09-11 01:42:22 +0900

    [FIX] join table 참조 설정

--- src/main/java/com/example/digging/domain/entity/UserHasPosts.java
+++ src/main/java/com/example/digging/domain/entity/UserHasPosts.java
@@ -23,7 +23,7 @@ public class UserHasPosts {
     private Posts posts;
 
     @ManyToOne
-//    @JoinColumn(name="user_id", referencedColumnName="id")
+    @JoinColumn(name="user_id", referencedColumnName="user_id")
     private User user;
 
 }
